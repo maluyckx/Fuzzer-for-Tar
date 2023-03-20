cedaad4c7e77fcca840e71f16277dc17702eacf8
maluyckx <Marco.Luyckx@ulb.ac.be>
2023-03-20 22:27:59 +0100

    setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fuzzer
+
+
+https://www.gnu.org/software/tar/manual/html_node/Standard.html
+
+
+probably split every header part into a different file (AKA clean code my G)
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2) {
+        printf("be gone THOT !");
+        return -1;
+    }
+    
+    return 0;
+}
